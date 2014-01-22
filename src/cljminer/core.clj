029d5862c738d4418a220c75088715b19a18b93f
Lily <lily@voidlily.net>
2014-01-22 13:40:06 -0800

    skeleton

--- src/cljminer/core.clj
+++ src/cljminer/core.clj
@@ -1,9 +1,50 @@
 (ns cljminer.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def difficulty (atom nil))
+(def nonce-seq (iterate inc 0N))
+
+;;; Given a sequence starting from 0
+;;; Convert each entry to base 36
+;;; Hash it and make a commit object
+;;; See if that commit objects meets the difficulty
+
+;;; To parallelize:
+;;; Some kind of signal sending to kill stuff?
+;;; pmap deflates
+;;; (->> nonce-seq (pmap craft-commit) (filter within-difficulty) first)
+;;; future work for competion
+;;; wrap this in a future with a 15 minute timeout?
+;;; if i don't find one in 15 minutes, shutdown-agents and start over?
+
+(defn commit-body [tree parent timestamp nonce]
+  (str "tree " tree "\n"
+       "parent " parent "\n"
+       "author CTF user <me@example.com> " timestamp " +0000" "\n"
+       "committer CTF user <me@example.com> " timestamp " +0000" "\n"
+       "\n"
+       "Give me a Gitcoin" "\n"
+       "\n"
+       nonce))
+
+(defn nonce-to-string [x]
+  (Integer/toString x 36))
+
+(defn get-difficulty []
+  5)
+
+(defn get-repo []
+  5)
+
+(defn get-base-commit [repo]
+  5)
+
+(defn find-nonce [difficulty]
+  5)
 
 (defn -main []
+  (let [difficulty (get-difficulty)
+        repo (get-repo)
+        base-commit (get-base-commit repo)]
+    (find-nonce difficulty)
+    )
   )
